d4fb2123b1edea8a8c6c9385ff39344f765aaa28
Gustavo Lourenço <up202306578@up.pt>
2025-03-18 11:43:17 +0000

    Added new things

---
+++ Code/Menu.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "Menu.h"
+
+
+void Menu_intro() {
+    std::cout<<"Welcome to our project"<<std::endl;
+}
---
+++ Code/Menu.h
@@ -0,0 +1,7 @@
+
+#ifndef MENU_H
+#define MENU_H
+
+void Menu_intro();
+
+#endif //MENU_H
