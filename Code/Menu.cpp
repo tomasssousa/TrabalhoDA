798da7f5d3a3a5ec5eee07f8924f78a4b895b891
Gustavo Lourenço <up202306578@up.pt>
2025-03-20 22:41:00 +0000

    Initial implementation of Menu(for now it just shows some options, and does not receive inputs)

--- Code/Menu.cpp
+++ Code/Menu.cpp
@@ -3,6 +3,14 @@
 
 
 void Menu_intro() {
-    std::cout<<"Welcome to our project"<<std::endl;
-    std::cout<<"Do you want to load the graph? "<<std::endl;
+    std::cout << "----------------------------------------" << std::endl;
+    std::cout<<"          Welcome to our project!     "<<std::endl;
+    std::cout<< "----------------------------------------" << std::endl;
+    std::cout<<"1. Load and Parse the given files "<<std::endl;
+    std::cout<<"2. Display the information about the Urban Map"<<std::endl;
+    std::cout<<"3. Plan a route (after choosing this option you will be sent to another menu)"<<std::endl;
+    std::cout<<"4. Execute Batch Mode"<<std::endl;
+    std::cout<<"About"<<std::endl;
+    std::cout<<"Exit"<<std::endl;
+    std::cout<< "----------------------------------------" << std::endl;
 }
