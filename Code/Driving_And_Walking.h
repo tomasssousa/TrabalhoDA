2cda9023a74b2868c9e57e3d3373a68c8239b75b
Tomás <up202303524@up.pt>
2025-03-30 16:52:57 +0100

    Adding Walking Dijkstra

--- Code/Driving_And_Walking.h
+++ Code/Driving_And_Walking.h
@@ -18,6 +18,103 @@
 using namespace std;
 
 /**
+* @brief This function is responsible for the edge relaxation that takes place during the Dijkstra algorithm.
+* @param edge The edge to be either crossed or not.
+* @return The boolean saying if the DrivingDist got changed or not.
+*/
+template <class T>
+bool walkingrelax(Edge<T> *edge) { // d[u] + w(u,v) < d[v]
+    if (edge->getOrig()->getWalkingDist() + edge->getWalkingWeight() < edge->getDest()->getWalkingDist()) { // we have found a better way to reach v
+        edge->getDest()->setWalkingDist(edge->getOrig()->getWalkingDist() + edge->getWalkingWeight()); // d[v] = d[u] + w(u,v)
+        edge->getDest()->setPath(edge); // set the predecessor of v to u; in this case the edge from u to v
+        return true;
+    }
+    return false;
+}
+
+/**
+* @brief Implementation of custom Dijkstra algorithm.
+*
+* Custom Dijkstra that takes two additional vectors and is able to avoid specific nodes and edges.
+* @param g The graph to be explored.
+* @param origin The id of the node from which the algorithm will start.
+* @param rn The id of the nodes to be ignored.
+* @param re The ids of the starting and destination vertices that define an edge to be ignored.
+*/
+template <class T>
+void walkingdijkstra(Graph<T> * g, const int &origin, vector<int> *rn ,vector<pair<int,int>> *re) {
+    /// Initialize the vertices
+    for(auto v : g->getVertexSet()) {
+        v->setWalkingDist(INF);
+        v->setPath(nullptr);
+        v->setVisited(false);
+    }
+    ///remove nodes from the iteration. If the vector is empty it does nothing.
+    for(int n : rn){
+        auto v = g->findVertex(n);
+        v->setVisited(true);
+    }
+    ///remove edges from the iteration. If the vector is empty it does nothing.
+    for(pair<int,int> en : re){
+        auto v = g->findVertex(en.first);
+        for(auto e : v->getAdj()){
+            if(e->getDest()==g->findVertex(en.second)){
+                e->setWalkingDist(INF);
+            }
+        }
+    }
+    auto s = g->findVertex(origin); ///< The node from which to start the algorithm.
+    s->setWalkingDist(0);
+
+    MutablePriorityQueue<Vertex<T>> q; ///< PriorityQueue to be used during the algorithm.
+    q.insert(s);
+    while( ! q.empty() ) {
+        auto v = q.extractMin();
+        v->setVisited(true);
+        for(auto e : v->getAdj()) {
+            auto oldDist = e->getDest()->getWalkingDist();
+            if (relax(e) && e->getDest()->isVisited()==false){
+                if (oldDist == INF) {
+                    q.insert(e->getDest());
+                }
+                else {
+                    q.decreaseKey(e->getDest());
+                }
+            }
+        }
+    }
+}
+
+/**
+* @brief Gives the path from a node to the other, if there is one.
+* @param g The graph where the nodes belong to.
+* @param origin The starting node.
+* @param dest The detination node.
+* @return A vector with either the path from a node to the other or an empty vector in the case where there isn't a path.
+*/
+template <class T>
+static std::vector<T> getWalkingPath(Graph<T> * g, const int &origin, const int &dest) {
+    std::vector<T> res; ///< The vector where the result will be placed
+    auto v = g->findVertex(dest);
+    if (v == nullptr || v->getWalkingDist() == INF) { // missing or disconnected
+        return res;
+    }
+    res.push_back(v->getId());
+    while(v->getPath() != nullptr) {
+        v = v->getPath()->getOrig();
+        res.push_back(v->getId());
+    }
+    reverse(res.begin(), res.end());
+    if(res.empty() || res[0] != origin) {
+        //std::cout << "Origin not found!!" << std::endl;
+        //return empty vector
+        std::vector<T> empty;
+        return empty;
+    }
+    return res;
+}
+
+/**
  * @brief Finds valid parking nodes that allow walking to the destination within the max walking time.
  *
  * @tparam T Data type of the graph nodes.
@@ -32,7 +129,7 @@ vector<int> findValidParkingNodes(Graph<T> *g, int origin, int destination, int 
     vector<int> parkingNodes;
     for (auto v : g->getVertexSet()) {
         if (v->getParking() == 1 && v->getId() != origin && v->getId() != destination) {
-            dijkstra(g, v->getId(), {}, {});
+            walkingdijkstra(g, v->getId(), {}, {});
             if (g->findVertex(destination)->getWalkingDist() <= maxWalkTime) {
                 parkingNodes.push_back(v->getId());
             }
@@ -74,8 +171,8 @@ vector<vector<T>> DrivingWalking(Graph<T> *g, int origin, int destination, int m
         auto drivingRoutes = Driving_only(g, origin, parkingNode, &avoidNodes, &avoidEdges, {});
         if (drivingRoutes[0].empty()) continue;
 
-        dijkstra(g, parkingNode, {}, {});
-        vector<T> walkingRoute = getPath<T>(g, parkingNode, destination);
+        walkingdijkstra(g, parkingNode, {}, {});
+        vector<T> walkingRoute = getWalkingPath<T>(g, parkingNode, destination);
 
         if (walkingRoute.empty() || g->findVertex(destination)->getWalkingDist() > maxWalkTime) continue;
 
